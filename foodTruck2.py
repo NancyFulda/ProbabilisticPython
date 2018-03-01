Fixed averaging error, increased complexity in foodTruck2.py

foodTruck2.py
@@ -1,7 +1,7 @@
 from inferencedriver import InferenceDriver
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 # ----------------------------------
@@ -20,16 +20,28 @@ import matplotlib.pyplot as plt
 def generate_dataset(size, types, preferences):
         data = []
         for i in range(size):
-            truck = np.random.choice(types)
-            chance_of_eating = preferences[types.index(truck)]
-            eats = np.random.choice([1, 0], p=[chance_of_eating, 1.0-chance_of_eating])
-            data.append((truck, eats))
+
+            #FIRST OBSERVATION
+            #student sees a truck, and eats from it based on
+            #preferences in isolation
+            truck1= np.random.choice(types)
+            chance_of_eating = preferences[types.index(truck1)]
+            eats1 = np.random.choice([1, 0], p=[chance_of_eating, 1.0-chance_of_eating])
+            #SECOND OBSERVATION
+            #student sees a second truck, and eats from it based on
+            #preferences of the two trucks in comparison to each other
+            remaining_types = types[:]
+            remaining_types.remove(truck1)
+            truck2= np.random.choice(remaining_types)
+            chance_of_eating = preferences[types.index(truck2)]/( preferences[types.index(truck1)] + preferences[types.index(truck2)])
+            eats2 = np.random.choice([1, 0], p=[chance_of_eating, 1.0-chance_of_eating])
+            data.append((truck1, eats1, truck2, eats2))
         return data
 
 
 TRUCK_TYPES = ['Korean', 'Lebanese', 'Mexican']
 TRUE_PREFERENCES = [.1, .5, .9]
-DATASET_SIZE = 100
+DATASET_SIZE = 1000
 data = generate_dataset(size=DATASET_SIZE, types=TRUCK_TYPES, preferences=TRUE_PREFERENCES)
 
 # ----------------------------------
@@ -45,13 +57,23 @@ def model( pp ):
     preferences = [korean_pref, lebanese_pref, mexican_pref]
 
     for i in range(len(data)):
-        #set up the experiment: student sees a single food truck.
-        truck_type = pp.choice(name="truck", elements=TRUCK_TYPES, loop_iter=i)
+        #set up the experiment: student sees two food trucks.
+        truck_type1 = pp.choice(name="truck1", elements=TRUCK_TYPES, loop_iter=i)
+        remaining_truck_types = TRUCK_TYPES[:]
+        remaining_truck_types.remove(truck_type1)
+        truck_type2 = pp.choice(name="truck2", elements=remaining_truck_types, loop_iter=i)
 
-        #Student will eat it with probability preferences...
-        chance_of_eating = preferences[TRUCK_TYPES.index(truck_type)]
+        #Student will eat the first with probability based on overall
+        #preferences
+        chance_of_eating = preferences[TRUCK_TYPES.index(truck_type1)]
         probs = [chance_of_eating, 1.0-chance_of_eating]
-        eats = pp.choice(name="eats", elements=[1,0], p=probs, loop_iter=i)
+        eats1 = pp.choice(name="eats1", elements=[1,0], p=probs, loop_iter=i)
+        
+        #Student will eat the first with probability based on relative
+        #preferences of the two trucks
+        chance_of_eating = preferences[TRUCK_TYPES.index(truck_type2)]/( preferences[TRUCK_TYPES.index(truck_type1)] + preferences[TRUCK_TYPES.index(truck_type2)])
+        probs = [chance_of_eating, 1.0-chance_of_eating]
+        eats1 = pp.choice(name="eats1", elements=[1,0], p=probs, loop_iter=i)
 
 # ----------------------------------
 # Run inference
@@ -61,8 +83,10 @@ driver = InferenceDriver(model)
 
 # condition
 for i in range(len(data)):
-    driver.condition(label="truck-{}".format(i), value=data[i][0])
-    driver.condition(label="eats-{}".format(i), value=data[i][1])
+    driver.condition(label="truck1-{}".format(i), value=data[i][0])
+    driver.condition(label="eats1-{}".format(i), value=data[i][1])
+    driver.condition(label="truck2-{}".format(i), value=data[i][2])
+    driver.condition(label="eats2-{}".format(i), value=data[i][3])
 
 # init and establish priors
 driver.init_model()
@@ -71,10 +95,10 @@ driver.prior(label="lebanese-0", value=.5)
 driver.prior(label="mexican-0", value=.9)
 
 # burn in
-#driver.burn_in(steps=100)
+driver.burn_in(steps=1000)
 
 # run inference
-driver.run_inference(interval=5, samples=500)
+driver.run_inference(interval=10, samples=100)
 mydata = driver.return_plt_data(keys=["korean","lebanese","mexican"])
 
 # print estimated preferences
@@ -94,7 +118,8 @@ for key in data_pref.keys():
 print("Data:", data_pref)
 
 # graph your likelihood
-#driver.graph_ll()
+driver.graph_ll()
+plt.gcf().clear()
 
 #plot the inference data
 plt.plot(range(len(mydata['korean'])),mydata['korean'],label='Korean')
@@ -102,6 +127,5 @@ plt.plot(range(len(mydata['lebanese'])),mydata['lebanese'],label='Lebanese')
 plt.plot(range(len(mydata['mexican'])),mydata['mexican'],label='Mexican')
 plt.legend()
 plt.savefig("results.png")
-
-print mydata['mexican'][10]
+plt.show()
 